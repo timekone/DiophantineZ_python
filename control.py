01570903379f3d52cbb99e36bca17b5c1cbd64e4
Dmitry Belogub <dmitry.belogub@gmail.com>
2017-12-20 19:25:48 +0200

    Comments have been added.

--- control.py
+++ control.py
@@ -7,21 +7,23 @@ import csv
 
 def get_input_size(input_name):
     input_arr = solver.read_file('./Input/' + input_name)
-    return str(len(input_arr))+'x'+str(len(input_arr[0]))
+    return [len(input_arr), len(input_arr[0])]
 
 
-main_output_file = open('./Output/_Main_output.csv', 'a', newline='')
+#main_output_file = open('./Output/_Main_output.csv', 'a', newline='')
 for filename in os.listdir('./Input/'):
+    main_output_file = open('./Output/_Main_output.csv', 'a', newline='')
     start_time = time.time()
     result = solver.control_main('./Input/' + filename)
     end_time = time.time()
-
     input_size = get_input_size(filename)
     # string_to_main = input_size + ' - ' + str(end_time-start_time)
     # print(string_to_main + ' ' + str(datetime.datetime.today()))
     # main_output_file.write(string_to_main + '\n')
     writer = csv.writer(main_output_file)
-    writer.writerow([input_size, str(end_time-start_time)])
-    with open("./Output/" + filename[:-4] + '(out).txt', 'w') as output_file:
-        for vector in result:
-            output_file.write(str(vector)+'\n')
+    writer.writerow([str(input_size[0]), str(input_size[1]), str(end_time-start_time)])
+    print(input_size[0], input_size[1])
+    main_output_file.close()
+    # with open("./Output/" + filename[:-4] + '(out).txt', 'w') as output_file:
+    #     for vector in result:
+    #         output_file.write(str(vector)+'\n')
--- solver.py
+++ solver.py
@@ -2,6 +2,11 @@ from functools import reduce
 
 
 def read_file(input_name):
+    """
+    read input matrix
+    :param input_name: file name
+    :return: input_arr: list of vectors
+    """
     input_arr = []
     with open(input_name) as input_file:
         for line in input_file:
@@ -14,51 +19,63 @@ def read_file(input_name):
 
 
 def create_pre_basis(equation):
+    """
+    create prebasis for given equation
+    :param equation: vector
+    :return: pre_basis: list of vectors
+    """
     pre_basis = []
-    i_not_zero = 0
-    for i in range(0, len(equation)):
+    i_not_zero = 0  # placement of first coordinate which not equals zero
+    for i in range(0, len(equation)):  # search for i_not_zero
         if equation[i] != 0:
             i_not_zero = i
             break
-    for i in range(0, len(equation)):
-        if i == i_not_zero:
+    for i in range(0, len(equation)):  # iterate through equation coefficients
+        if i == i_not_zero:  # skip
             continue
-        pre_basis_vector_i = [0]*len(equation)
-        pre_basis_vector_i[i_not_zero] = equation[i] * -1
-        pre_basis_vector_i[i] = equation[i_not_zero]
-        pre_basis.append(pre_basis_vector_i)
+        pre_basis_vector_i = [0]*len(equation)  # initialize with zeroes
+        pre_basis_vector_i[i_not_zero] = equation[i] * -1  # put inverted coefficient on place of first not zero
+        pre_basis_vector_i[i] = equation[i_not_zero]  # replace coefficient with first not zero
+        pre_basis.append(pre_basis_vector_i)  # for each coefficient add new vector to prebasis
     return pre_basis
 
 
 def substitute(equation, pre_basis):
+    """
+    substitute vectors of prebasis to given equation
+    :param equation: list of integers
+    :param pre_basis: list of vectors
+    :return: list of integers
+    """
     result = []
     for vector in pre_basis:
-        one_vector_result = 0
+        one_vector_result = 0  # value of substitution of single prebasis vector
         for c in range(0, len(equation)):
             one_vector_result += vector[c]*equation[c]
-        result.append(one_vector_result)
+        result.append(one_vector_result)  # add for each prebasis vector
     return result
 
 
 def multiply_pre_basis(big_pre_basis, small_pre_basis):
     """
-    multiplies pre basis from equation gained from substitution and pre basis from initial equation
-    :param big_pre_basis:
-    :param small_pre_basis:
-    :return: result
+    multiplies pre basis from equation gained from substitution and pre basis from initial equation(main prebasis)
+    :param big_pre_basis: from initial(main prebasis)
+    :param small_pre_basis: gained from substitution
+    :return: result: list of vectors. List size equals to size of small_pre_basis(gained from substitution)
     """
     result = []
     for small_vector in small_pre_basis:
-        one_vector_result = [0] * len(big_pre_basis[0])
-        for i_small_vector in range(0, len(small_vector)):
-            if small_vector[i_small_vector] != 0:
+        one_vector_result = [0] * len(big_pre_basis[0])  # result from one vector of substitution prebasis
+        for i_small_vector in range(0, len(small_vector)):  # for each coefficient of vector from substituion prebasis
+            if small_vector[i_small_vector] != 0:  # if coefficient not zero
                 for c in range(len(big_pre_basis[0])):
+                    # multiply coefficient of vector from small prebasis(from substitution) with each vector from main prebasis(big), sum vectors
                     one_vector_result[c] += big_pre_basis[i_small_vector][c] * small_vector[i_small_vector]
         result.append(one_vector_result)
     return result
 
 
-def GCD(a, b):  # It takes too long!
+def GCD(a, b):  # Greatest Common Divisor
     if b == 0:
         return a
     else:
@@ -66,29 +83,38 @@ def GCD(a, b):  # It takes too long!
 
 
 def simplify(ar):
-    ar2 = []
+    """
+    simplifying vectors of matrix
+    :param ar: matrix of vectors
+    :return: ar2: simplified matrix
+    """
+    ar2 = []  # for storing result(faster than deleting)
     for y in ar:
-        d = reduce(GCD, y)  # move from here, after redundant
+        d = reduce(GCD, y)  # searching for greatest common divisor
         if d != 1 and d != 0:
-            y = list(map(lambda t: t//d, y))
-        ar2.append(y)
+            y = list(map(lambda t: t//d, y))  # dividing by GCD
+        ar2.append(y)  # append to answer
     return ar2
 
 
 def solv(input_arr):
-    pre_basis_main = create_pre_basis(input_arr[0])
-    for Li in range(1, len(input_arr)):
-        Y = substitute(input_arr[Li], pre_basis_main)
-        pre_basis_Y = create_pre_basis(Y)
-        pre_basis_main = multiply_pre_basis(pre_basis_main, pre_basis_Y)
-        pre_basis_main = simplify(pre_basis_main)
+    pre_basis_main = create_pre_basis(input_arr[0])  # create prebasis for the first equation
+    for Li in range(1, len(input_arr)):  # iterate through other equations
+        Y = substitute(input_arr[Li], pre_basis_main)  # substitute vectors of prebasis to equation Li
+        pre_basis_Y = create_pre_basis(Y)  # create prebasis from result of substitution
+        pre_basis_main = multiply_pre_basis(pre_basis_main, pre_basis_Y)  # get new main prebasis
+        pre_basis_main = simplify(pre_basis_main)  # simplify vectors of prebasis if possible
     return pre_basis_main
 
 
-def control_main(input_name):
+def control_main(input_name):  # for using as module
     return solv(read_file(input_name))
 
 
-if __name__ == "__main__":
-    file = 'input(old).txt'
+if __name__ == "__main__":  # if launched standalone
+    file = 'generated.txt'
+    import time
+    start_time = time.time()
     print(solv(read_file(file)))
+    end_time = time.time()
+    print(end_time-start_time)
